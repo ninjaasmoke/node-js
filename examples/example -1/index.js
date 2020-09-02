fdfdd170a25dc5ec0b95b3944d16b56c8c41ebb2
Nithin Sai K J <nithins674@gmail.com>
2020-09-02 17:06:22 +0530

    async callback

--- examples/example -1/index.js
+++ examples/example -1/index.js
@@ -1,15 +1,38 @@
 // import { perimeter, area } from "./rect";
+// var rect = require("./rect")
+
+// function solveRect(l, b) {
+//     console.log("Solving for rectangle: l = " + l + " and b = " + b);
+
+//     if (l <= 0 || b <= 0) {
+//         console.log("Rect dimensions not proper");
+//     } else {
+//         console.log("Rect perimeter: " + rect.perimeter(l, b));
+//         console.log("Rect area: " + rect.area(l, b));
+//     }
+// }
+
+// solveRect(2, 1);
+// solveRect(2, 2);
+// solveRect(0, -12);
+
+
 var rect = require("./rect")
 
 function solveRect(l, b) {
     console.log("Solving for rectangle: l = " + l + " and b = " + b);
 
-    if (l <= 0 || b <= 0) {
-        console.log("Rect dimensions not proper");
-    } else {
-        console.log("Rect perimeter: " + rect.perimeter(l, b));
-        console.log("Rect area: " + rect.area(l, b));
-    }
+    rect(l, b, (err, rectangle) => {
+        if (err) {
+            console.log("\nERROR: ", err.message);
+        } else {
+            console.log("\nL : " + l + " and B : " + b);
+            console.log("Area : " + rectangle.area());
+            console.log("Perimeter : " + rectangle.perimeter());
+        }
+    });
+
+    console.log("\nAfter async call to rect\n");
 }
 
 solveRect(2, 1);
--- examples/example -1/rect.js
+++ examples/example -1/rect.js
@@ -1,3 +1,16 @@
-exports.perimeter = (x, y) => { return 2 * (x + y) }
-
-exports.area = (x, y) => { return x * y }
+module.exports = (x, y, callback) => {
+    if (x <= 0 || y <= 0) {
+        setTimeout(() => {
+            callback(new Error("Dimensions can't be less than or equal to zero"), null);
+        }, 2000);
+    } else {
+        setTimeout(() => {
+            callback(null, {
+                // perimeter: (x, y) => { return 2 * (x + y) }, 
+                perimeter: () => { return 2 * (x + y) },  // closure
+                // area: (x, y) => { return x * y },
+                area: () => { return x * y }, // closure
+            });
+        }, 2000);
+    }
+}
