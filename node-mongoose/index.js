ac6054ddf016e6854a89de4562d190099bc4632c
Nithin Sai K J <nithins674@gmail.com>
2020-09-08 20:11:38 +0530

    mongoose part 3

--- node-mongoose/index.js
+++ node-mongoose/index.js
@@ -16,11 +16,25 @@ connect.then((db) => {
             console.log('\nDish\n');
             console.log(dish);
 
-            return Dishes.find({}).exec();
+            return Dishes.findByIdAndUpdate(dish._id, {
+                $set: { description: 'Updated Text' }
+            },
+                { new: true }
+            ).exec();
         })
-        .then((dishes) => {
-            console.log('\nAll dishes\n');
-            console.log(dishes);
+        .then((dish) => {
+            console.log(dish);
+
+            dish.comments.push({
+                rating: 5,
+                comment: 'I\'m getting a sinking feeling',
+                author: 'Leonardo di Caprio'
+            });
+
+            return dish.save();
+
+        }).then((dish) => {
+            console.log(dish);
 
             return Dishes.remove({});
         })
