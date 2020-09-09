628e73ec6960fa777c1cd6cceab5f7fb3034ca20
Nithin Sai K J <nithins674@gmail.com>
2020-09-09 19:50:01 +0530

    express node api with mongoose and mongodb server

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -12,6 +12,19 @@ var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
 
+const mongoose = require('mongoose');
+
+const Dishes = require('./models/dishes');
+
+const url = 'mongodb://localhost:27017/conFusion';
+const connect = mongoose.connect(url);
+
+connect.then((db) => {
+  console.log('Connected correctly to server');
+}, (err) => {
+  console.error(err);
+});
+
 
 var app = express();
 
