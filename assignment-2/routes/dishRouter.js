a879841507e702bb252f820a39304eb3052624da
Nithin Sai K J <nithins674@gmail.com>
2020-09-09 21:48:56 +0530

    assignment 2

---
+++ assignment-2/routes/dishRouter.js
@@ -0,0 +1,227 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const Dishes = require('../models/dishes');
+const { json } = require('express');
+
+const dishRouter = express.Router();
+
+dishRouter.use(bodyParser.json());
+
+dishRouter.route('/')
+    .get((req, res, next) => {
+        // modification of res is carried from app.all
+        Dishes.find({})
+            .then((dishes) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(dishes);
+            }, (err) => next(err))
+            .catch((err) => next(err));
+    })
+    .post((req, res, next) => {
+        Dishes.create(req.body)
+            .then((dish) => {
+                console.log('Dish created: ', dish);
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(dish);
+            }, (err) => next(err))
+            .catch((err) => next(err));
+    })
+    .put((req, res, next) => {
+        res.statusCode = 403;
+        res.end('PUT operation not supported on dishes!!!');
+    })
+    .delete((req, res, next) => {
+        Dishes.remove({})
+            .then((resp) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(resp);
+            }, (err) => next(err))
+            .catch((err) => next(err));
+    });
+
+// // With params
+
+dishRouter.route('/:dishId')
+    .get((req, res, next) => {
+        // modification of res is carried from app.all
+        Dishes.findById(req.params.dishId)
+            .then((dish) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(dish);
+            }, (err) => next(err))
+            .catch((err) => next(err));
+    })
+    .post((req, res, next) => {
+        res.statusCode = 403;
+        res.end('POST operation not supported on dish with ID!!!');
+    })
+    .put((req, res, next) => {
+        Dishes.findByIdAndUpdate(req.params.dishId, {
+            $set: req.body
+        }, { new: true })
+            .then((dish) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(dish);
+            }, (err) => next(err))
+            .catch((err) => next(err));
+    })
+    .delete((req, res, next) => {
+        Dishes.findByIdAndRemove(req.params.dishId)
+            .then((resp) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(resp);
+            }, (err) => next(err))
+            .catch((err) => next(err));
+    });
+
+
+dishRouter.route('/:dishId/comments')
+    .get((req, res, next) => {
+        // modification of res is carried from app.all
+        Dishes.findById(req.params.dishId)
+            .then((dish) => {
+                if (dish != null) {
+                    res.statusCode = 200;
+                    res.setHeader('Content-Type', 'application/json');
+                    res.json(dish.comments);
+                } else {
+                    err = new Error('Dish ' + req.params.dishId + ' not found');
+                    err.statusCode = 404;
+                    return next(err); // see in app.js error handling function which handles this return
+                }
+            }, (err) => next(err))
+            .catch((err) => next(err));
+    })
+    .post((req, res, next) => {
+        Dishes.findById(req.params.dishId)
+            .then((dish) => {
+                if (dish != null) {
+                    dish.comments.push(req.body);
+                    dish.save()
+                        .then((dish) => {
+                            res.statusCode = 200;
+                            res.setHeader('Content-Type', 'application/json');
+                            res.json(dish);
+                        }, (err) => next(err));
+                } else {
+                    err = new Error('Dish ' + req.params.dishId + ' not found');
+                    err.statusCode = 404;
+                    return next(err); // see in app.js error handling function which handles this return
+                }
+            }, (err) => next(err))
+            .catch((err) => next(err));
+    })
+    .put((req, res, next) => {
+        res.statusCode = 403;
+        res.end('PUT operation not supported on dishes/' + req.params.dishId + '/comments');
+    })
+    .delete((req, res, next) => {
+        Dishes.findById(req.params.dishId)
+            .then((dish) => {
+                if (dish != null) {
+                    for (var i = (dish.comments.length - 1); i >= 0; i--) { // removes all comments one by one
+                        dish.comments.id(dish.comments[i]._id).remove(); // accessing the subdocument
+                    }
+                    dish.save()
+                        .then((dish) => {
+                            res.statusCode = 200;
+                            res.setHeader('Content-Type', 'application/json');
+                            res.json(dish);
+                        }, (err) => next(err));
+                } else {
+                    err = new Error('Dish ' + req.params.dishId + ' not found');
+                    err.statusCode = 404;
+                    return next(err); // see in app.js error handling function which handles this return
+                }
+            }, (err) => next(err))
+            .catch((err) => next(err));
+    });
+
+// // With params
+
+dishRouter.route('/:dishId/comments/:commentID')
+    .get((req, res, next) => {
+        // modification of res is carried from app.all
+        Dishes.findById(req.params.dishId)
+            .then((dish) => { // 3 possibilities
+                if (dish != null && dish.comments.id(req.params.commentID) != null) {
+                    res.statusCode = 200;
+                    res.setHeader('Content-Type', 'application/json');
+                    res.json(dish.comments.id(req.params.commentID));
+                } else if (dish == null) {
+                    err = new Error('Dish ' + req.params.dishId + ' not found');
+                    err.statusCode = 404;
+                    return next(err); // see in app.js error handling function which handles this return
+                } else {
+                    err = new Error('Comment ' + req.params.commentID + ' not found');
+                    err.statusCode = 404;
+                    return next(err);
+                }
+            }, (err) => next(err))
+            .catch((err) => next(err));
+    })
+    .post((req, res, next) => {
+        res.statusCode = 403;
+        res.end('POST operation not supported on /dishes/' + req.params.dishId + '/comments/' + req.params.commentID);
+    })
+    .put((req, res, next) => {
+        Dishes.findById(req.params.dishId)
+            .then((dish) => { // 3 possibilities
+                if (dish != null && dish.comments.id(req.params.commentID) != null) {
+                    if (req.body.rating) {
+                        dish.comments.id(req.params.commentID).rating = req.body.rating;
+                    }
+                    if (req.body.comment) {
+                        dish.comments.id(req.params.commentID).comment = req.body.comment;
+                    }
+                    dish.save()
+                        .then((dish) => {
+                            res.statusCode = 200;
+                            res.setHeader('Content-Type', 'application/json');
+                            res.json(dish);
+                        }, (err) => next(err));
+                } else if (dish == null) {
+                    err = new Error('Dish ' + req.params.dishId + ' not found');
+                    err.statusCode = 404;
+                    return next(err); // see in app.js error handling function which handles this return
+                } else {
+                    err = new Error('Comment ' + req.params.commentID + ' not found');
+                    err.statusCode = 404;
+                    return next(err);
+                }
+            }, (err) => next(err))
+            .catch((err) => next(err));
+    })
+    .delete((req, res, next) => {
+        Dishes.findById(req.params.dishId)
+            .then((dish) => {
+                if (dish != null && dish.comments.id(req.params.commentID) != null) {
+                    dish.comments.id(dish.comments.id(req.params.commentID)._id).remove(); // accessing the subdocument
+                    dish.save()
+                        .then((dish) => {
+                            res.statusCode = 200;
+                            res.setHeader('Content-Type', 'application/json');
+                            res.json(dish);
+                        }, (err) => next(err));
+                } else if (dish == null) {
+                    err = new Error('Dish ' + req.params.dishId + ' not found');
+                    err.statusCode = 404;
+                    return next(err); // see in app.js error handling function which handles this return
+                } else {
+                    err = new Error('Comment ' + req.params.commentID + ' not found');
+                    err.statusCode = 404;
+                    return next(err);
+                }
+            }, (err) => next(err))
+            .catch((err) => next(err));
+    });
+
+module.exports = dishRouter;
---
+++ assignment-2/routes/leaderRouter.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const Leaders = require('../models/leaders');
+
+const leaderRouter = express.Router();
+
+leaderRouter.use(bodyParser.json());
+
+leaderRouter.route('/')
+    .get((req, res, next) => {
+        Leaders.find({})
+            .then((leaders) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(leaders);
+            }, (err) => next(err))
+            .catch((err) => next(err));
+    })
+    .post((req, res, next) => {
+        Leaders.create(req.body)
+            .then((leader) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(leader);
+            }, (err) => next(err))
+            .catch((err) => next(err));
+    })
+    .put((req, res, next) => {
+        res.statusCode = 403;
+        res.end('PUT operation not supported on leaders!!!');
+    })
+    .delete((req, res, next) => {
+        Leaders.remove({})
+            .then((response) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(response);
+            }, (err) => next(err))
+            .catch((err) => next(err));
+    });
+
+// // With params
+
+leaderRouter.route('/:leaderID')
+    .get((req, res, next) => {
+        Leaders.findById(req.params.leaderID)
+            .then((leader) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(leader);
+            }, (err) => next(err))
+            .catch((err) => next(err));
+    })
+    .post((req, res, next) => {
+        res.statusCode = 403;
+        res.end('POST operation not supported on leader with ID!!!');
+    })
+    .put((req, res, next) => {
+        Leaders.findByIdAndUpdate(req.params.leaderID, {
+            $set: req.body
+        }, { new: true })
+            .then((leader) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(leader);
+            }, (err) => next(err))
+            .catch((err) => next(err));
+    })
+    .delete((req, res, next) => {
+        Leaders.findByIdAndRemove(req.params.leaderID)
+            .then((resp) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(resp);
+            }, (err) => next(err))
+            .catch((err) => next(err));
+    });
+
+
+module.exports = leaderRouter;
